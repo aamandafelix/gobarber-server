2fd51b963e01148bfedc084e118243c2104d0f40
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-29 17:13:19 -0300

    Implement create user functionality

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -5,11 +5,11 @@ import Appointment from '../models/Appointment';
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
   public async findByDate(date: Date): Promise<Appointment | null> {
-    const findAppoint = await this.findOne({
+    const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppoint || null;
+    return findAppointment || null;
   }
 }
 
