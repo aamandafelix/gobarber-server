7e437bc4908b263b1e6fd4fb165f6abd6c2e9628
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-30 16:25:03 -0300

    Implement upload user avatar functionality

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-
 import express from 'express';
+
+import uploadConfig from './config/upload';
 import routes from './routes';
 
 import './database';
@@ -8,6 +9,8 @@ import './database';
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
