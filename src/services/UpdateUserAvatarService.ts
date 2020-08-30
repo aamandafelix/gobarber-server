8700a9c5ea999485b3d2ceb36374223903ac4202
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-08-30 16:56:57 -0300

    Implement error handling

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 import uploadConfig from '../config/upload';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   user_id: string;
@@ -17,13 +18,12 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
-    // se ja tem avatar, remove o arquivo antes de adicionar novo avatar
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath); // pega status do arquivo
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
       if (userAvatarFileExists) {
         await fs.promises.unlink(userAvatarFilePath);
