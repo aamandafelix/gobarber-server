b2498a38f35bc7111e8ab8f9981f4548a47b02d4
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-24 20:28:48 -0300

    Create dev mail provider using ethereal and nodemailer

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 
 app.use(routes);
 
