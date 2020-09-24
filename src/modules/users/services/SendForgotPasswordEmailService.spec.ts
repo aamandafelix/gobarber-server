48bd8a77a117a8c6e133cdb61175d61000f7d5b9
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-23 22:11:10 -0300

    Use TDD to create recover password service functionalities

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,29 @@
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmailService', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@mail.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
