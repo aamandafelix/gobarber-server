333125bdf6b56ba6342d9b185d2688150bbd73c8
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-24 21:24:53 -0300

    Change mail provider implementation for using files templates

--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import handlebars from 'handlebars';
 
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
@@ -5,10 +6,14 @@ import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTempalte = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTempalte = handlebars.compile(templateFileContent);
 
     return parseTempalte(variables);
   }
