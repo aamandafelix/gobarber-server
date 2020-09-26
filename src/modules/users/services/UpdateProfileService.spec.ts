b1f44cd775018f4e681a8dc837bcd84a0755f449
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-26 14:48:52 -0300

    Use TDD to create profile update service functionalities

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfile: UpdateProfileService;
+
+describe('UpdateProfileService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfile = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('should be able to update the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'John F. Doe',
+      email: 'johnfdoe@mail.com',
+    });
+
+    expect(updatedUser.name).toBe('John F. Doe');
+    expect(updatedUser.email).toBe('johnfdoe@mail.com');
+  });
+});
---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private iHashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, name, email }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfileService;
