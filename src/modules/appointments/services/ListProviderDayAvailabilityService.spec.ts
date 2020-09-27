adec0d53c4673e1cf0553031c6c672b0e0cc9ca6
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-27 01:36:41 -0300

    Use TDD to create provider's availability of day service functionalities

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderDayAvailabilityService', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user-id',
-      date: new Date(2020, 4, 14, 8, 0, 0),
+      date: new Date(2020, 4, 14, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user-id',
-      date: new Date(2020, 4, 14, 10, 0, 0),
+      date: new Date(2020, 4, 14, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 14, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -33,9 +37,12 @@ describe('ListProviderDayAvailabilityService', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
