8dcaa189c7314185cfebef4b412c42105f630725
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-27 02:27:07 -0300

    Use TDD to create appointment creation service functionalities

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -21,12 +21,12 @@ describe('CreateAppointmentService', () => {
 
     const appointment = await createAppointment.execute({
       date: new Date(2020, 4, 10, 13),
-      provider_id: '123123123',
-      user_id: '1111111',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123123');
+    expect(appointment.provider_id).toBe('provider-id');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
@@ -38,15 +38,15 @@ describe('CreateAppointmentService', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '123123123',
-      user_id: '1111111',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '123123123',
-        user_id: '1111111',
+        provider_id: 'provider-id',
+        user_id: 'user-id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
@@ -59,8 +59,22 @@ describe('CreateAppointmentService', () => {
     await expect(
       createAppointment.execute({
         date: new Date(2020, 4, 10, 11),
-        provider_id: '123123123',
-        user_id: '1111111',
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 13),
+        provider_id: 'provider-id',
+        user_id: 'provider-id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
