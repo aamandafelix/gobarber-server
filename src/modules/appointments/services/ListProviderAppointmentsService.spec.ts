4a51da6a4052c73d649eac23ae8aaf3cd7a2ad13
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-28 12:50:55 -0300

    Use TDD to create appointments listing service functionalities

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the provider appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 14, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 14, 15, 0, 0),
+    });
+
+    const availability = await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      day: 14,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(availability).toEqual([appointment1, appointment2]);
+  });
+});
