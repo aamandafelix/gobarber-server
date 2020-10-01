dbfa674da7f18859818589bfb55a1027ab77874d
Amanda Bezerra <amandabs_contato@hotmail.com>
2020-09-30 23:26:58 -0300

    Create amazon s3 provider for image storing in production

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -19,7 +19,7 @@ class SESMailProvider implements IMailProvider {
     this.client = nodemailer.createTransport({
       SES: new aws.SES({
         apiVersion: '2010-12-01',
-        region: 'us-east-1',
+        region: process.env.AWS_DEFAULT_REGION,
       }),
     });
   }
---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+import aws, { S3 } from 'aws-sdk';
+
+import uploadConfig from '@config/upload';
+
+import IStorageProvider from '../models/IStorageProvider';
+
+class S3StorageProvider implements IStorageProvider {
+  private cliente: S3;
+
+  constructor() {
+    this.cliente = new aws.S3({
+      region: process.env.AWS_DEFAULT_REGION,
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    await this.cliente
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.cliente
+      .deleteObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export default S3StorageProvider;
